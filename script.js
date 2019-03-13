9d815610edba20d57dd3dc6d2e4ec4262a8897ef
a16thegu <37794246+a16thegu@users.noreply.github.com>
2019-03-13 17:11:24 +0100

    Adding validation of which JS library the user wants to use to animatio the rendering effects.
    Has not worked that good as I would have hoped.

---
+++ script.js
@@ -0,0 +1,14 @@
+function printGame(cards, category, library) {
+    console.log("Hej: " + library);
+
+    if (library.text = "jQuery"){
+        console.log("Hello, World jQuery!");
+        alert("!");
+    } 
+    else if (library.text = "AnimeJS"){
+        console.log("Hello, World AnimeJS!");
+    }
+    else {
+        console.log("Hello, World AniJS!");
+    }
+}
