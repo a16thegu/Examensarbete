c89c8b4640a04ab3807cff3594618b69521f8ef9
a16thegu <37794246+a16thegu@users.noreply.github.com>
2019-03-13 18:39:24 +0100

    Prints out card objects.
    The card objects just disappears efter button is clicked, checking with Google why the objects are not sticking around.

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-function printGame(cards, category, library) {
-    console.log("Hej: " + library);
-
-    if (library.text = "jQuery"){
-        console.log("Hello, World jQuery!");
-        alert("!");
+function startGame() {
+    var cards = document.getElementById("cards");
+    var category = document.getElementById("category");
+    var library = document.getElementById("library");
+    
+    if (library.value == "jQuery"){
+        printCardsJQUERY(cards.value, category.value);
     } 
-    else if (library.text = "AnimeJS"){
-        console.log("Hello, World AnimeJS!");
+    else if (library.value == "AnimeJS"){
+        printCardsANIMEJS(cards.value, category.value);
     }
     else {
-        console.log("Hello, World AniJS!");
+        printCardsANIJS(cards.value, category.value);
     }
+}
+
+
+function printCardsJQUERY(cards, category) {
+    for (var i = 0; i < cards; i++){
+        var div = document.createElement("div");
+        div.id = i + 1;
+        div.className = "card";
+        document.getElementById("game").appendChild(div);
+    }
+}
+
+function printCardsANIMEJS(cards, category) {
+    alert("Values: " + cards + " " + category);
+}
+
+function printCardsANIJS(cards, category) {
+    alert("Values: " + cards + " " + category);
 }
