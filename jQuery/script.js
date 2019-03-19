81d8a6b64c1e021c65d869296441d617c58f65c3
a16thegu <37794246+a16thegu@users.noreply.github.com>
2019-03-19 22:55:50 +0100

    Added county information as objects.

--- jQuery/script.js
+++ jQuery/script.js
@@ -5,5 +5,137 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 function start(e){
-    console.log(e.explicitOriginalTarget.attributes[2]);
+    console.log(e.explicitOriginalTarget.attributes[2].nodeValue);
 }
+
+
+var county = {
+    id: "SE-K",
+    context: {
+        name: "Blekinge",
+        image: "",
+        text: ""
+    },
+    id: "SE-I",
+    context: {
+        name: "Gotland",
+        image: "",
+        text: ""
+    },
+    id: "SE-X",
+    context: {
+        name: "Gävleborg",
+        image: "",
+        text: ""
+    },
+    id: "SE-N",
+    context: {
+        name: "Halland",
+        image: "",
+        text: ""
+    },
+    id: "SE-Z",
+    context: {
+        name: "Jämtland",
+        image: "",
+        text: ""
+    },
+    id: "SE-F",
+    context: {
+        name: "Jönköping",
+        image: "",
+        text: ""
+    },
+    id: "SE-H",
+    context: {
+        name: "Kalmar",
+        image: "",
+        text: ""
+    },
+    id: "SE-W",
+    context: {
+        name: "Dalarna",
+        image: "",
+        text: ""
+    },
+    id: "SE-G",
+    context: {
+        name: "Kronoberg",
+        image: "",
+        text: ""
+    },
+    id: "SE-BD",
+    context: {
+        name: "Norrbotten",
+        image: "",
+        text: ""
+    },
+    id: "SE-E",
+    context: {
+        name: "Östergötland",
+        image: "",
+        text: ""
+    },
+    id: "SE-T",
+    context: {
+        name: "Örebro",
+        image: "",
+        text: ""
+    },
+    id: "SE-D",
+    context: {
+        name: "Södermanland",
+        image: "",
+        text: ""
+    },
+    id: "SE-M",
+    context: {
+        name: "Skåne",
+        image: "",
+        text: ""
+    },
+    id: "SE-AB",
+    context: {
+        name: "Stockholm",
+        image: "",
+        text: ""
+    },
+    id: "SE-C",
+    context: {
+        name: "Uppsala",
+        image: "",
+        text: ""
+    },
+    id: "SE-AC",
+    context: {
+        name: "Västerbotten",
+        image: "",
+        text: ""
+    },
+    id: "SE-O",
+    context: {
+        name: "Västra Götaland",
+        image: "",
+        text: ""
+    },
+    id: "SE-S",
+    context: {
+        name: "Värmland",
+        image: "",
+        text: ""
+    },
+    id: "SE-U",
+    context: {
+        name: "Västmanland",
+        image: "",
+        text: ""
+    },
+    id: "SE-Y",
+    context: {
+        name: "Västernorrland",
+        image: "",
+        text: ""
+    },
+    
+    
+};
