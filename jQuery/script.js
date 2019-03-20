cdaef959dba9a22cf33bc77e2280593ff6625d79
a16thegu <37794246+a16thegu@users.noreply.github.com>
2019-03-21 00:09:15 +0100

    Added Home-button
    Local storage will know which the previous clicked county was, so it can color it back to black and color the currenttarget county to red, but the home-button will alwasy be black.

--- jQuery/script.js
+++ jQuery/script.js
@@ -9,7 +9,12 @@ function start(e){
     var headline = document.getElementById("headline");
     var text = document.getElementById("text");
     var image = document.getElementById("image");
-    
+
+    if(localStorage.getItem("County") === null){
+        localStorage.setItem("County", "Home")
+    };
+
+    var previous = localStorage.getItem("County");
 
     /*$("#content").slideUp(function(){
         for(var i = 0; i < countys.length; i++){
@@ -23,44 +28,55 @@ function start(e){
     $("#content").slideDown(1500);*/
 
     $("#content").animate({
-        height : "0px"
+        height: "0px",
+        width: "0px"
     }, "slow", function(){
         for(var i = 0; i < countys.length; i++){
             if (countys[i][0] == county){
                 headline.innerHTML = countys[i][1];
-                text.innerHTML = "";
-                image.innerHTML = "";
+                text.innerHTML = countys[i][2];
+                image.innerHTML = countys[i][3];
             }
-        }
+        };
+
+        if (county == "Home"){
+            document.getElementById(county).style.fill = "black";
+            document.getElementById(previous).style.fill = "black";
+        } 
+        else {
+            document.getElementById(previous).style.fill = "black";
+            document.getElementById(county).style.fill = "#c50101";
+        };
     });
     $("#content").animate({
-        height : "800px"
+        height: "800px",
+        width: "700px"
     }, "slow");
 
-    
+    localStorage.setItem("County", county);
 }
 
 var countys = [
-    ["SE", "Welcome to the interactive Sweden map!"]
-    ["SE-K","Blekinge","",""],
-    ["SE-I","Gotland","",""],
-    ["SE-X","Gävleborg","",""],
-    ["SE-N","Halland","",""],
-    ["SE-Z","Jämtland","", ""],
-    ["SE-F","Jönköping","",""],
-    ["SE-H","Kalmar","",""],
-    ["SE-W","Dalarna","",""],
-    ["SE-G","Kronoberg","",""],
-    ["SE-BD","Norrbotten","",""],
-    ["SE-E","Östergötland","",""],
-    ["SE-T","Örebro","",""],
-    ["SE-D","Södermanland","",""],
-    ["SE-M","Skåne","",""],
-    ["SE-AB","Stockholm","",""],
-    ["SE-C","Uppsala","",""],
-    ["SE-AC","Västerbotten","",""],
-    ["SE-O","Västra Götaland","",""],
-    ["SE-S","Värmland","",""],
-    ["SE-U","Västmanland","",""],
-    ["SE-Y","Västernorrland","",""]
+    ["Home", "Welcome to the interactive Sweden map!", "This is a graduation projekt at University of Skövde.", ""],
+    ["SE-K", "Blekinge", "", ""],
+    ["SE-I", "Gotland", "", ""],
+    ["SE-X", "Gävleborg", "", ""],
+    ["SE-N", "Halland", "", ""],
+    ["SE-Z", "Jämtland", "", ""],
+    ["SE-F", "Jönköping", "", ""],
+    ["SE-H", "Kalmar", "", ""],
+    ["SE-W", "Dalarna", "", ""],
+    ["SE-G", "Kronoberg", "", ""],
+    ["SE-BD", "Norrbotten", "", ""],
+    ["SE-E", "Östergötland", "", ""],
+    ["SE-T", "Örebro", "", ""],
+    ["SE-D", "Södermanland", "", ""],
+    ["SE-M", "Skåne", "", ""],
+    ["SE-AB", "Stockholm", "", ""],
+    ["SE-C", "Uppsala", "", ""],
+    ["SE-AC", "Västerbotten", "", ""],
+    ["SE-O", "Västra Götaland", "", ""],
+    ["SE-S", "Värmland", "", ""],
+    ["SE-U", "Västmanland", "", ""],
+    ["SE-Y", "Västernorrland", "", ""]
 ];
