c50da45a324dc518889c2dbfd209f612291db336
a16thegu <37794246+a16thegu@users.noreply.github.com>
2019-03-20 23:06:27 +0100

    Changed county to an array instead of objects.
    Added animation to the county information card and a welcome headline.

--- jQuery/script.js
+++ jQuery/script.js
@@ -5,137 +5,62 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 function start(e){
-    console.log(e.explicitOriginalTarget.attributes[2].nodeValue);
+    var county = e.explicitOriginalTarget.attributes[2].nodeValue;
+    var headline = document.getElementById("headline");
+    var text = document.getElementById("text");
+    var image = document.getElementById("image");
+    
+
+    /*$("#content").slideUp(function(){
+        for(var i = 0; i < countys.length; i++){
+            if (countys[i][0] == county){
+                headline.innerHTML = countys[i][1];
+                text.innerHTML = "";
+                image.innerHTML = "";
+            }
+        }
+    });
+    $("#content").slideDown(1500);*/
+
+    $("#content").animate({
+        height : "0px"
+    }, "slow", function(){
+        for(var i = 0; i < countys.length; i++){
+            if (countys[i][0] == county){
+                headline.innerHTML = countys[i][1];
+                text.innerHTML = "";
+                image.innerHTML = "";
+            }
+        }
+    });
+    $("#content").animate({
+        height : "800px"
+    }, "slow");
+
+    
 }
 
-
-var county = {
-    id: "SE-K",
-    context: {
-        name: "Blekinge",
-        image: "",
-        text: ""
-    },
-    id: "SE-I",
-    context: {
-        name: "Gotland",
-        image: "",
-        text: ""
-    },
-    id: "SE-X",
-    context: {
-        name: "Gävleborg",
-        image: "",
-        text: ""
-    },
-    id: "SE-N",
-    context: {
-        name: "Halland",
-        image: "",
-        text: ""
-    },
-    id: "SE-Z",
-    context: {
-        name: "Jämtland",
-        image: "",
-        text: ""
-    },
-    id: "SE-F",
-    context: {
-        name: "Jönköping",
-        image: "",
-        text: ""
-    },
-    id: "SE-H",
-    context: {
-        name: "Kalmar",
-        image: "",
-        text: ""
-    },
-    id: "SE-W",
-    context: {
-        name: "Dalarna",
-        image: "",
-        text: ""
-    },
-    id: "SE-G",
-    context: {
-        name: "Kronoberg",
-        image: "",
-        text: ""
-    },
-    id: "SE-BD",
-    context: {
-        name: "Norrbotten",
-        image: "",
-        text: ""
-    },
-    id: "SE-E",
-    context: {
-        name: "Östergötland",
-        image: "",
-        text: ""
-    },
-    id: "SE-T",
-    context: {
-        name: "Örebro",
-        image: "",
-        text: ""
-    },
-    id: "SE-D",
-    context: {
-        name: "Södermanland",
-        image: "",
-        text: ""
-    },
-    id: "SE-M",
-    context: {
-        name: "Skåne",
-        image: "",
-        text: ""
-    },
-    id: "SE-AB",
-    context: {
-        name: "Stockholm",
-        image: "",
-        text: ""
-    },
-    id: "SE-C",
-    context: {
-        name: "Uppsala",
-        image: "",
-        text: ""
-    },
-    id: "SE-AC",
-    context: {
-        name: "Västerbotten",
-        image: "",
-        text: ""
-    },
-    id: "SE-O",
-    context: {
-        name: "Västra Götaland",
-        image: "",
-        text: ""
-    },
-    id: "SE-S",
-    context: {
-        name: "Värmland",
-        image: "",
-        text: ""
-    },
-    id: "SE-U",
-    context: {
-        name: "Västmanland",
-        image: "",
-        text: ""
-    },
-    id: "SE-Y",
-    context: {
-        name: "Västernorrland",
-        image: "",
-        text: ""
-    },
-    
-    
-};
+var countys = [
+    ["SE", "Welcome to the interactive Sweden map!"]
+    ["SE-K","Blekinge","",""],
+    ["SE-I","Gotland","",""],
+    ["SE-X","Gävleborg","",""],
+    ["SE-N","Halland","",""],
+    ["SE-Z","Jämtland","", ""],
+    ["SE-F","Jönköping","",""],
+    ["SE-H","Kalmar","",""],
+    ["SE-W","Dalarna","",""],
+    ["SE-G","Kronoberg","",""],
+    ["SE-BD","Norrbotten","",""],
+    ["SE-E","Östergötland","",""],
+    ["SE-T","Örebro","",""],
+    ["SE-D","Södermanland","",""],
+    ["SE-M","Skåne","",""],
+    ["SE-AB","Stockholm","",""],
+    ["SE-C","Uppsala","",""],
+    ["SE-AC","Västerbotten","",""],
+    ["SE-O","Västra Götaland","",""],
+    ["SE-S","Värmland","",""],
+    ["SE-U","Västmanland","",""],
+    ["SE-Y","Västernorrland","",""]
+];
